d82e9c1c288cc0ee5bd324adb796b970500f67bb
ReveMbeng <reve@getadok.com>
2018-03-22 21:18:16 +0100

    first commit

---
+++ app/src/main/java/com/example/lmbongue/malta/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.lmbongue.malta;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lmbongue.malta.view.BlankFragment;
+
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+       BlankFragment blankFragment = (BlankFragment) getFragmentManager().findFragmentById(R.id.frag);
+    }
+}
